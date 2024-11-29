21fd0611394f45c5dd53b1337089a439dbdb643d
Hussein Alhajam <hussein.alhajam@ontariotechu.net>
2024-11-28 20:31:25 -0500

    removed an incorrect import in auth controller

--- backend/controllers/auth_controller.py
+++ backend/controllers/auth_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, request, jsonify, session, url_for
-from backend.services import auth_service
+from services import auth_service
 from services.auth_service import register_user, login_user, handle_google_callback
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from extensions import google
